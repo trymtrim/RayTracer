3f26c4f7a0005c648ecec31294b8d58ed5b22a52
Trym Rannem <trymtrim@live.no>
2020-11-27 07:37:14 +0100

    .

--- RayTracerTestBed_New/Source/Scene.cs
+++ RayTracerTestBed_New/Source/Scene.cs
@@ -46,20 +46,25 @@ namespace RayTracerTestBed
 			Material planeMaterial = new Material(0.8f);
 			materials.Add(planeMaterial);
 
-			Mesh sphere1 = new Sphere(new Vector3(-2.5f, 1.0f, 6.0f), 1.0f); //Grey sphere
+			Mesh sphere1 = new Sphere(new Vector3(-4.0f, 1.0f, 6.0f), 1.0f); //Grey sphere
 			meshes.Add(sphere1);
 			Material sphere1Material = new Material(new Vector3(0.7f, 0.7f, 0.7f));
 			materials.Add(sphere1Material);
 
-			Mesh sphere2 = new Sphere(new Vector3(2.5f, -2.5f, 9.0f), 2.0f); //Large blue sphere higher up
+			Mesh sphere2 = new Sphere(new Vector3(1.0f, -2.5f, 9.0f), 2.0f); //Large blue sphere higher up
 			meshes.Add(sphere2);
 			Material sphere2Material = new Material(new Vector3(0.0f, 0.0f, 1.0f));
 			materials.Add(sphere2Material);
 
-			Mesh sphere3 = new Sphere(new Vector3(-0.5f, 0.5f, 11.0f), 1.5f); //Semi-specular sphere behind
+			Mesh sphere3 = new Sphere(new Vector3(-2.0f, 0.5f, 11.0f), 1.5f); //Specular sphere
 			meshes.Add(sphere3);
 			Material sphere3Material = new Material(new Vector3(1.0f, 1.0f, 1.0f), 0.9f); //REMINDER: Put white to get full mirror effect
 			materials.Add(sphere3Material);
+
+			Mesh sphere4 = new Sphere(new Vector3(3.0f, 0.5f, 6.0f), 1.5f); //Semi-specular red sphere
+			meshes.Add(sphere4);
+			Material sphere4Material = new Material(new Vector3(1.0f, 0.0f, 0.0f), 0.75f); //REMINDER: Put white to get full mirror effect
+			materials.Add(sphere4Material);
 		}
 
 		private void SpawnMap2()
